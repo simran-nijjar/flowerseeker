f8a235fbe130f2015a17cfceac9b90c136e996ae
Simran Nijjar <simran.nijjar@hotmail.com>
2022-10-07 21:01:35 -0700

    Add Play Game, Options, and Help buttons to main menu screen. Add flower image to main menu screen.

--- app/src/main/java/ca/cmpt276/assignment3/MainMenu.java
+++ app/src/main/java/ca/cmpt276/assignment3/MainMenu.java
@@ -1,6 +1,15 @@
 package ca.cmpt276.assignment3;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainMenu extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainmenu);
+    }
+
+
 }
